73f0a7c099fe3964957006e83b411837744f3f2c
spleshka <spleshka@gmail.com>
2013-06-25 17:17:42 +0300

    Updated documentation.

--- expire.api.php
+++ expire.api.php
@@ -9,7 +9,11 @@
  * Provides possibility to flush pages for external cache storages.
  *
  * @param $urls
- *   List of internal urls that should be flushed.
+ *   List of absolute URLs that should be flushed.
+ *
+ * @param $wildcards
+ *   Array with wildcards implementations.
+ *   Indicates whether should be executed wildcard cache flush.
  *
  * @param $object_type
  *  Name of object type ('node', 'comment', 'user', etc).
@@ -19,10 +23,10 @@
  *
  * @see expire.api.inc
  */
-function hook_expire_cache($urls, $object_type, $object) {
+function hook_expire_cache($urls, $wildcards, $object_type, $object) {
   foreach ($urls as $url) {
     $full_path = url($url, array('absolute' => TRUE));
-    purge_url($full_path);
+    purge_url($full_path, $wildcards);
   }
 }
 
@@ -30,7 +34,7 @@ function hook_expire_cache($urls, $object_type, $object) {
  * Provides possibility to change urls before they are expired.
  *
  * @param $urls
- *   List of internal urls that should be flushed.
+ *   List of internal paths or absolute URLs that should be flushed.
  *
  * @param $object_type
  *  Name of object type ('node', 'comment', 'user', etc).
@@ -38,10 +42,13 @@ function hook_expire_cache($urls, $object_type, $object) {
  * @param $object
  *   Object (node, comment, user, etc) for which expiration is executes.
  *
+ * @param $absolute_urls_passed
+ *   Indicates whether absolute URLs or internal paths were passed.
+ *
  * @see expire.api.inc
  */
-function hook_expire_cache_alter($urls, $object_type, $object) {
-  if ($object_type == 'node') {
+function hook_expire_cache_alter($urls, $object_type, $object, $absolute_urls_passed) {
+  if ($object_type == 'node' && !$absolute_urls_passed) {
     unset($urls['node-' . $object->nid]);
     $urls['example'] = 'custom_page/' . $object->uid . '/' . $object->nid;
   }
